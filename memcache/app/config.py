318fee91e5df722b926a546d8f999b3484d7c37e
Yizhou Shen <yizhou.shen@outlook.com>
2022-10-09 03:52:38 -0400

    more skeletons, display all keys
    implemented page that display all available keys stored in the database

---
+++ memcache/app/config.py
@@ -0,0 +1,4 @@
+db_config = {'user': 'dbadmin',
+             'password': 'admin1234',
+             'host': '127.0.0.1',
+             'database': 'ece1779project'}
--- memcache/app/main.py
+++ memcache/app/main.py
@@ -1,10 +1,33 @@
 
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, g
 from app import webapp, memcache
 from flask import json
 
+import mysql.connector
+from app.config import db_config
+import sys
 
-@webapp.route('/')
+def connect_to_database():
+    return mysql.connector.connect(user=db_config['user'],
+                                   password=db_config['password'],
+                                   host=db_config['host'],
+                                   database=db_config['database'])
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+    return db
+
+@webapp.teardown_appcontext
+def teardown_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+# @webapp.route('/')
+@webapp.route('/',methods=['GET'])
+@webapp.route('/main',methods=['GET'])
 def main():
     return render_template("main.html")
 
@@ -42,3 +65,38 @@ def put():
 
     return response
 
+########################################################
+
+@webapp.route('/upload', methods=['GET'])
+def upload():
+    # implement /upload
+    return render_template("main.html")
+
+@webapp.route('/show_image', methods=['GET'])
+def show_image():
+    # implement /show_image
+    return render_template("main.html")
+
+
+@webapp.route('/all_keys', methods=['GET'])
+def all_keys():
+    cnx = get_db()
+
+    cursor = cnx.cursor()
+
+    query = "SELECT * FROM imagelist"
+
+    cursor.execute(query)
+
+    return render_template("keylist.html",title="ImageID List", cursor=cursor)
+
+
+@webapp.route('/config_mem_cache', methods=['GET'])
+def config_mem_cache():
+    # implement /config_mem_cache
+    return render_template("main.html")
+
+@webapp.route('/statistics', methods=['GET'])
+def statistics():
+    # implement /statistics
+    return render_template("main.html")
