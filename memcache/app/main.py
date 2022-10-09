f66c2ed37cd221dd3bcf2b8867caab962d89f6d8
Yizhou Shen <yizhou.shen@outlook.com>
2022-10-09 05:29:36 -0400

    show image working without mem-cache

--- memcache/app/main.py
+++ memcache/app/main.py
@@ -72,10 +72,37 @@ def upload():
     # implement /upload
     return render_template("main.html")
 
-@webapp.route('/show_image', methods=['GET'])
-def show_image():
-    # implement /show_image
-    return render_template("main.html")
+@webapp.route('/image_form', methods=['GET'])
+def image_form():
+    return render_template("image_form.html", title="Select Image Key")
+
+@webapp.route('/image_display', methods=['POST'])
+def image_display():
+    # implement /image_display
+    if 'image_key' not in request.form:
+        return "Need a image key"
+
+    image_key = request.form.get('image_key')
+
+    if image_key == '':
+        return "Need a image key"
+
+    cnx = get_db()
+    cursor = cnx.cursor()
+
+    query = ''' SELECT ImagePath FROM imagelist
+                WHERE ImageID = %s'''
+
+    cursor.execute(query, (image_key,))
+    row = cursor.fetchone()
+    
+    if row == None:
+        return "No such image"
+
+    # image_path = os.path.join('static/images', row[1])
+    image_path = row[0]
+
+    return render_template("image_display.html", title="Image Display", image_path=image_path)
 
 
 @webapp.route('/all_keys', methods=['GET'])
@@ -88,7 +115,7 @@ def all_keys():
 
     cursor.execute(query)
 
-    return render_template("keylist.html",title="ImageID List", cursor=cursor)
+    return render_template("keylist.html", title="ImageID List", cursor=cursor)
 
 
 @webapp.route('/config_mem_cache', methods=['GET'])
