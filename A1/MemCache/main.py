0cbbfbbcea36418157f182b7f61a53aff52713fe
Zihe <zihe.jia@mail.utoronto.ca>
2022-10-15 17:41:04 -0400

    Complete all database reads, add status record variables

--- A1/MemCache/main.py
+++ A1/MemCache/main.py
@@ -4,10 +4,7 @@ import random
 
 # for database
 import mysql.connector
-# from flask import render_template, url_for, request, g
-# from WebFrontend import webapp, memcache
 from config import db_config
-DROP_APPROACH = 0
 
 # database prepare
 def connect_to_database():
@@ -18,9 +15,6 @@ def connect_to_database():
                                    auth_plugin='mysql_native_password')
 
 def get_db():
-    # db = getattr(g, '_database', None)
-    # if db is None:
-    #     db = g._database = connect_to_database()
     db = connect_to_database()
     return db
 
@@ -37,25 +31,31 @@ class PicMemCache(object):
     def __init__(self, memCacheCapacity=None) -> None:
         # 用户设定的MemCache大小，需从数据库读取
 
-        # 单独写一个函数，用来和db交互 or 直接写在功能函数中？
+        # 从db读取memCacheCapacity，但此处没有对数据合法性进行验证
         cnx = get_db()
         cursor = cnx.cursor()
-
         query = "SELECT Capacity FROM configuration WHERE id = 1"
-
         cursor.execute(query)
-        # row = cursor.fetchone()
-
         # self.memCacheCapacity = memCacheCapacity
         self.memCacheCapacity = cursor.fetchone()[0]
-        # 当前MemCache容量 = 占用情况
+
+        # 统计变量
+        self.drop_approach = 1
         self.currentMemCache = 0
+        self.ItemNum = 0
+        self.TotalRequestNum = 0
+        self.MissNum = 0
+        self.HitNum = 0
+        self.GetPicRequestNum = 0
+
         self.MC = OrderedDict()
+        print("memCacheCapacity", self.memCacheCapacity)
 
     def drop_specific_pic(self, keyID):
         # invalidateKey(key) to drop a specific key.
         self.currentMemCache -= getsizeof(self.MC[keyID])
         self.MC.pop(keyID)
+        self.ItemNum -= 1
         # 返回成功与否
 
     def drop_pic(self, approach=1):
@@ -81,26 +81,41 @@ class PicMemCache(object):
         # 加入新图片后，没有超过MemCache总容量
             self.MC[keyID] = picString
             self.currentMemCache += getsizeof(picString)
+            self.ItemNum += 1
         else:
             # print('超了')
 
         # 加入新图片后，超过了MemCache总容量，需要丢掉图片，存入新图片
+
+            cnx = get_db()
+            cursor = cnx.cursor()
+            query = "SELECT ReplacePolicy FROM configuration WHERE id = 1"
+            cursor.execute(query)
+            self.drop_approach = cursor.fetchone()[0]
+            print("drop_approach: ", self.drop_approach)
+
             while self.currentMemCache + getsizeof(picString) > self.memCacheCapacity:
-                self.drop_pic(DROP_APPROACH)
+                self.drop_pic(self.drop_approach)
             self.MC[keyID] = picString
             self.currentMemCache += getsizeof(picString)
+            self.ItemNum += 1
             print('Memcache set key {}'.format(keyID))
 
     def get_pic(self, keyID):
         # 当用户看图时，调用此函数
         # 唯一写入memCache的情况
+
+        self.GetPicRequestNum += 1
+
         if self.MC[keyID] != -1:
             # 调用的图片就在MemCache
             self.MC.move_to_end(key=keyID, last=True)
             # 需要：将图片发送到前端
             return self.MC[keyID]
+            self.HitNum += 1
         else:
             # 调用的图片不在MemCache
+            self.MissNum += 1
             print("MemCache无此图片")
             # 从前端接收图片string，然后存入memCache
             # self.put_pic(keyID, picStrin) —— 这里picStrin参数怎么处理？
@@ -110,6 +125,7 @@ class PicMemCache(object):
         self.MC.clear()
         # if self.MC == None: 这里需要判断空嘛
         self.currentMemCache = 0
+        self.ItemNum = 0
 
 
     def refreshConfiguration(self, newMemCacheCapacity):
@@ -117,8 +133,15 @@ class PicMemCache(object):
             # 最好在前端验证，不能在数据库中存非正常值
             print("MemCacheCapacity应该大于等于0，本次更改失败")
         else:
+            cnx = get_db()
+            cursor = cnx.cursor()
+            query = "SELECT ReplacePolicy FROM configuration WHERE id = 1"
+            cursor.execute(query)
+            self.drop_approach = cursor.fetchone()[0]
+            print("drop_approach: ", self.drop_approach)
+
             while newMemCacheCapacity < self.currentMemCache:
-                self.drop_pic(DROP_APPROACH)
+                self.drop_pic(self.drop_approach)
             self.memCacheCapacity = newMemCacheCapacity
 
 
