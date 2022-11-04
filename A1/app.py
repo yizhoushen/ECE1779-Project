fae8d8e1ba7bf8720e7e19d9463311907d11beab
Yizhou Shen <yizhou.shen@outlook.com>
2022-11-03 23:09:13 -0400

    delete app.py

--- A1/app.py
+++
@@ -1,8 +0,0 @@
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-
-from WebFrontend import webapp as webfrontend
-from MemCache import webapp_memcache as memcache
-
-application = DispatcherMiddleware(webfrontend, {
-    '/memcache': memcache
-})
