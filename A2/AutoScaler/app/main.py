402dba5ba0481f57f47da82a423d2809024bcda6
Zihe <zihe.jia@mail.utoronto.ca>
2022-11-11 13:22:49 -0500

    add set_ratio function

--- A2/AutoScaler/app/main.py
+++ A2/AutoScaler/app/main.py
@@ -13,7 +13,6 @@ MIN_MISS_RATE_THRESHOLD = 0
 expand_ratio = 2
 shrink_ratio = 0.5
 
-# 需要前端进行合规性检查
 MAX_NUM_OF_INSTANCES = 8
 MIN_NUM_OF_INSTANCES = 1
 
@@ -73,8 +72,13 @@ def operate_instances(delta_of_instances = 0):
     else:
         print("Don't need to adjust instances!")
 
-def auto_bu_qidong():
-    # 等待信号变更为auto模式，重置标志位，启动
+def autoscaler_mode_change():
+    # 不能用while，占用线程
+    while(AUTO_SCALER_ENABLE):
+        # listen miss rate
+        delta_of_instances = get_instance_change(miss_rate)
+        operate_instances(delta_of_instances)
+    # check AUTO_SCALER_ENABLE变化
 
 @webapp_autoscaler.route('/')
 def main():
@@ -83,29 +87,71 @@ def main():
     #     1.1 若传来手动信号，则启动autoscaler功能函数
     #     1.2 若传来自动信号，则停止autoscaler功能函数 / 启动 非autoscaler功能
 
-    if (new_message != AUTO_SCALER_ENABLE):
-
-
-    return render_template("main.html")
+    # if (new_message != AUTO_SCALER_ENABLE):
+    #
+    #
+    # return render_template("main.html")
 
 
 @webapp_memcache.route('/set_autoscaler_mode', methods=['POST'])
 def set_autoscaler_mode():
     new_autoscaler_mode = request.form.get('autoscaler_mode')
-    if new_autoscaler_mode != AUTO_SCALER_ENABLE:
-        if new_autoscaler_mode == 1:
-            AUTO_SCALER_ENABLE = True
-            response = jsonify(success='True',
-                               message='Success! The mode of autoscaler is on.')
-        elif new_autoscaler_mode == 0:
-            AUTO_SCALER_ENABLE = False
-            response = jsonify(success='True',
-                               message='Success! The mode of autoscaler is off.')
-        else:
-            response = jsonify(success='False',
-                               message='Failure! Illegal parameters, the mode of autoscaler unchanged.')
+    if new_autoscaler_mode == 1:
+        AUTO_SCALER_ENABLE = True
+        autoscaler_mode_change()
+        response = jsonify(success='True',
+                           message='Success! The mode of autoscaler is on.')
+    elif new_autoscaler_mode == 0:
+        AUTO_SCALER_ENABLE = False
+        autoscaler_mode_change()
+        response = jsonify(success='True',
+                           message='Success! The mode of autoscaler is off.')
+    else:
+        response = jsonify(success='False',
+                           message='Failure! Illegal parameters, the mode of autoscaler unchanged.')
     return response
 
+@webapp_memcache.route('/set_ratio', methods=['POST'])
+def set_ratio():
+    ratio_type = request.form.get('ratio_type')
+    ratio_num = request.form.get('ratio_num')
+    # 潜在问题：ratio_num是string or float
+
+    if ratio_type not in ["expand", "shrink"]:
+        response = jsonify(success='False',
+                           message='Failure! The ratio_type is illegal.')
+    elif ratio_type == "expand":
+        if ratio_num <= 1:
+            response = jsonify(success='False',
+                               message='Failure! The ratio_num is illegal.')
+        else:
+            expand_ratio = ratio_num
+            response = jsonify(success='True',
+                               message='Success! The expand_ratio has changed to ' + str(expand_ratio))
+    elif ratio_type == "shrink":
+        if ratio_num <= 0 or ratio_num >= 1:
+            response = jsonify(success='False',
+                               message='Failure! The ratio_num is illegal.')
+        else:
+            shrink_ratio = ratio_num
+            response = jsonify(success='True',
+                               message='Success! The shrink_ratio has changed to ' + str(shrink_ratio))
+
+
+
+        AUTO_SCALER_ENABLE = True
+        autoscaler_mode_change()
+        response = jsonify(success='True',
+                           message='Success! The mode of autoscaler is on.')
+    elif new_autoscaler_mode == 0:
+        AUTO_SCALER_ENABLE = False
+        autoscaler_mode_change()
+        response = jsonify(success='True',
+                           message='Success! The mode of autoscaler is off.')
+    else:
+        response = jsonify(success='False',
+                           message='Failure! Illegal parameters, the mode of autoscaler unchanged.')
+    return response
 
 @webapp_memcache.route('/get_curr_autoscaler_mode', methods=['POST'])
 def get_curr_autoscaler_mode():
@@ -116,3 +162,5 @@ def get_curr_autoscaler_mode():
         response = jsonify(success='True',
                            message='The mode of autoscaler is off.')
     return response
+
+# 获取当前状态函数
