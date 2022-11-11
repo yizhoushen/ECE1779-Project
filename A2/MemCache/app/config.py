1b9046694a3dd88d9a7f4e835adb048129520d2b
Yizhou Shen <yizhou.shen@outlook.com>
2022-11-11 13:29:08 -0500

    aws run out of free tier, setup new config

--- A2/MemCache/app/config.py
+++ A2/MemCache/app/config.py
@@ -2,5 +2,5 @@
 # this database instance enables public access for local connection testing, remember to replace with a new S3 database instance without public access
 db_config = {'user': 'admin',
              'password': 'admin1234',
-             'host': 'database-test.cbb4fukguxcp.us-east-1.rds.amazonaws.com',
+             'host': 'database-new.c5xj5z6rnfm8.us-east-1.rds.amazonaws.com',
              'database': 'ece1779project'}
--- A2/WebFrontend/app/config.py
+++ A2/WebFrontend/app/config.py
@@ -2,10 +2,10 @@
 # this database instance enables public access for local connection testing, remember to replace with a new S3 database instance without public access
 db_config = {'user': 'admin',
              'password': 'admin1234',
-             'host': 'database-test.cbb4fukguxcp.us-east-1.rds.amazonaws.com',
+             'host': 'database-new.c5xj5z6rnfm8.us-east-1.rds.amazonaws.com',
              'database': 'ece1779project'}
 
 aws_access = {'aws_access_key_id': '<do not upload access key to github, change to your own>',
               'aws_secret_access_key': '<do not upload access key to github, change to your own>'}
 
-s3_bucket = {'name': 'bucket-ece1779-project'}
+s3_bucket = {'name': 'bucket-ece1779-project-new'}
