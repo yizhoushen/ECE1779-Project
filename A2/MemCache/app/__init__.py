0202d89f8819952d26d7ced5452787436f095f47
Yizhou Shen <yizhou.shen@outlook.com>
2022-11-20 19:31:00 -0500

    comment out std.log

--- A2/MemCache/app/__init__.py
+++ A2/MemCache/app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 import logging    # first of all import the module
 
-logging.basicConfig(filename='std.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
-logging.warning('This message will get logged on to a file')
+# logging.basicConfig(filename='std.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+# logging.warning('This message will get logged on to a file')
 
 webapp_memcache = Flask(__name__)
 memcache = {}
