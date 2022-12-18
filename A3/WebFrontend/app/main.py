7d46f4870aa96bc7c0f390b9f7dec254e9d833eb
Yizhou Shen <yizhou.shen@outlook.com>
2022-12-18 02:20:08 -0500

    get all tags and generate word cloud

--- A3/WebFrontend/app/main.py
+++ A3/WebFrontend/app/main.py
@@ -9,11 +9,17 @@ from app.config import db_config, s3_bucket, aws_access_key, aws_secret_key
 import sys
 
 import time
+import os
 
 import requests
 import base64
 
 import boto3
+from boto3.dynamodb.conditions import Key
+
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # os.chdir(os.path.abspath("./A1/WebFrontend"))
 
@@ -31,6 +37,50 @@ def get_db():
     if db is None:
         db = g._database = connect_to_database()
     return db
+
+def query_user_tag():
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('Images')
+
+    response = table.query(
+        KeyConditionExpression=Key('user_id').eq(app.userid),
+        ProjectionExpression="user_id, labels"
+    )
+    print(response)
+    records = []
+    for i in response['Items']:
+        for single_label in i['labels']:
+            records.append(single_label)
+    records = list(set(records))
+    return records
+
+def get_wordcloud_pic():
+    comment_words = ''
+    stopwords = set(STOPWORDS)
+
+    # process comment_words
+    tag_records = query_user_tag()
+    for tag in tag_records:
+        comment_words += tag + " "
+
+    print("comment_words: {}".format(comment_words))
+
+    wordcloud = WordCloud(width = 800, height = 800,
+				background_color ='white',
+				stopwords = stopwords,
+				min_font_size = 10).generate(comment_words)
+
+    # plot the WordCloud image					
+    plt.figure(figsize = (8, 8), facecolor = None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad = 0)
+
+    # plt.show()
+    wc_save_path = os.path.join("./WebFrontend/app/static/images", "wcgraph.png")
+    print("save path: {}".format(wc_save_path))
+
+    plt.savefig(wc_save_path)
 
 @webapp.teardown_appcontext
 def teardown_db(exception):
@@ -85,6 +135,19 @@ def profile():
         pass
     nickname = app.username
     return render_template("profile.html", title="Profile", nickname=nickname)
+
+@webapp.route('/wordcloud', methods=['GET', 'POST'])
+def get_wordcloud():
+    if app.userid == None:
+        return "Access Denied! Please Login!"
+    else:
+        pass
+    
+    get_wordcloud_pic()
+    # return "success"
+    image_path = os.path.join("./static/images", "wcgraph.png")
+    print("image_path: {}".format(image_path))
+    return render_template("wordcloud.html", image_path=image_path)
 
 # @webapp.route('/api_test', methods=['POST', 'GET'])
 # def test_api():
